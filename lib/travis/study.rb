756adebadb31a11b18d4333053f2d2c9a10aa526
paulo-felipe <alparakleto@gmail.com>
2022-02-21 16:04:44 -0300

    initial

---
+++ lib/travis/study.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "study/version"
+
+module Travis
+  module Study
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/travis/study_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Travis::Study do
+  it "has a version number" do
+    expect(Travis::Study::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
